f90489aec35fc936b616b08921bc3300fca96078
Sleepincoffee <cs781503542@qq.com>
2018-03-18 14:02:42 +0800

    完成页面及背景的部分修改

--- src/main/java/com/sprint/Application.java
+++ src/main/java/com/sprint/Application.java
@@ -2,6 +2,8 @@ package com.sprint;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Configuration; 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -12,7 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 @SpringBootApplication
 @EnableRedisHttpSession
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	
+	//Gradle WAR
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
         System.out.print("VIP管理系统启动成功");
